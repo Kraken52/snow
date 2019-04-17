870530acfdb98186e06789cfdd93c886d3242df7
Alex <mr.erc@inbox.ru>
2019-04-17 18:29:40 +0300

    two commit

--- snow.js
+++ snow.js
@@ -1,16 +1,58 @@
-function getId(name){
-  return document.getElementById(name);
+function getId(name,node=null){
+  if(node==null)
+    return document.getElementById(name);
+  else
+    return node.getElementById(name);
+}
+function getClass(name,node=null){
+  if(node==null)
+    return document.getElementsByClassName(name);
+  else
+    return node.getElementsByClassName(name);
+}
+function getTag(name,node=null){
+  if(node==null)
+    return document.getElementsByTagName(name);
+  else
+    return node.getElementsByTagName(name);
+}
+function getName(name,node=null){
+  if(node==null)
+    return document.getElementsByName(name);
+  else
+    return node.getElementsByName(name);
+}
+function getSelector(name,node=null){
+  if(node==null)
+    return document.querySelector(name);
+  else
+    return node.querySelector(name);
+}
+function getSelectors(name,node=null){
+  if(node==null)
+    return document.querySelectorAll(name);
+  else
+    return node.querySelectorAll(name);
 }
 
-function getClass(name){
-  return document.getElementsByClassName(name);
+function getParent(name){
+  return name.parentNode;
 }
 
-function getTag(name){
-  return document.getElementsByTagName(name);
+function getChilds(name){
+  return name.childNodes;
 }
 
+function getBefore(name){
+  do{
+       name = name.previousSibling;
+  }while(name.innerHTML==undefined);
+  return name;
+}
 
-function getName(name){
-  return document.getElementsByName(name);
+function getAfter(name){
+  do{
+       name = name.nextSibling;
+  }while(name.innerHTML==undefined);
+  return name;
 }
