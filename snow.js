43ee2d31de10fd14ebd6da695aea74386b67fc61
Alex <mr.erc@inbox.ru>
2019-04-17 16:59:17 +0300

    first comment

---
+++ snow.js
@@ -0,0 +1,16 @@
+function getId(name){
+  return document.getElementById(name);
+}
+
+function getClass(name){
+  return document.getElementsByClassName(name);
+}
+
+function getTag(name){
+  return document.getElementsByTagName(name);
+}
+
+
+function getName(name){
+  return document.getElementsByName(name);
+}
