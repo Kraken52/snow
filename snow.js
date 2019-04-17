20391e7012d56b9c9bbddd5bffdd16f4bff87542
Alex <mr.erc@inbox.ru>
2019-04-17 23:28:12 +0300

    new func

--- snow.js
+++ snow.js
@@ -56,3 +56,79 @@ function getAfter(name){
   }while(name.innerHTML==undefined);
   return name;
 }
+
+function getInfo(name){
+  return name.getBoundingClientRect();
+}
+
+function getAtt(name,node){
+  return node.getAttribute(name);
+}
+
+function setAtt(name,val,node){
+   node.setAttribute(name,val);
+}
+
+function removeAtt(name,node){
+  node.removeAttribute(name);
+}
+
+function hasAtt(name,node){
+  return node.hasAttribute(name);
+}
+
+function int(num){
+  return parseInt(num);
+}
+
+function float(num){
+  return parseFloat(num);
+}
+
+function string(str){
+  return toString(str);
+}
+
+function create(name){
+  return document.createElement(name);
+}
+
+function append(name,node){
+  node.appendChild(name);
+}
+
+function appendBefore(name,val,node=document){
+  if(node==document)
+    document.insertBefore(name,val);
+  else
+    node.insertBefore(name,val);
+}
+
+function replace(name, val, node=document){
+  if(node==document)
+    document.replaceChild(name,val);
+  else
+    node.replaceChild(name,val);
+}
+
+function remove(name,node=document){
+  if(node==document)
+    document.removeChild(name);
+  else
+    node.removeChild(name);
+}
+
+function clone(bool){
+  return name.cloneNode(bool);
+}
+function isEqual(val1,val2){
+  return val1.isEqualNode(val2);
+}
+
+function addEvent(name,func,bool,node){
+  node.addEventListener(name,func,bool);
+}
+
+function removeEvent(name,func,bool,node){
+  node.removeEventListener(name,func,bool);
+}
